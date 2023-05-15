4bf8cd60f488a673ba3b70e4bbc5152d777c1921
upendra ch <upendrach1612@gmail.com>
2023-05-14 20:54:03 -0500

    implemented code coverage for DepartmentApplication,Utils classes,lombok.config

--- src/test/java/com/departmentservice/department/DepartmentApplicationTests.java
+++ src/test/java/com/departmentservice/department/DepartmentApplicationTests.java
@@ -1,13 +1,42 @@
 package com.departmentservice.department;
 
+import com.departmentservice.department.Controller.Controller;
+import com.departmentservice.department.Controller.DepartmentController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DepartmentApplicationTests {
+
+	@Autowired
+	Controller controller;
+
+	@Autowired
+	DepartmentController departmentController;
 
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(controller);
+		Assertions.assertNotNull(departmentController);
+
+	}
+	@Test
+	public void applicationContextTest(){
+		DepartmentApplication.main(new String[]{});
+	}
+
+	@Test
+	public void controllerTest(){
+		assertThat(controller.checkingDepartmentService()).isEqualTo("The department service is working");
 	}
 
 }
---
+++ src/test/java/com/departmentservice/department/UtilsTest.java
@@ -0,0 +1,114 @@
+package com.departmentservice.department;
+
+import com.departmentservice.department.data.DepartmentEntity;
+import com.departmentservice.department.exception.NotFoundException;
+import com.departmentservice.department.model.DepartmentRequestModel;
+import com.departmentservice.department.model.DepartmentResponseModel;
+import com.departmentservice.department.shared.DepartmentDto;
+import com.departmentservice.department.shared.Utils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UtilsTest {
+    @Autowired
+    Utils utils;
+
+    @Test
+    public void getDepartmentResponseModelListTest() throws NotFoundException {
+        DepartmentDto departmentDto = new DepartmentDto(1, "name","city","state","country","zip code");
+        DepartmentDto departmentDto2 = new DepartmentDto(2, "name2","city2","state2","country2","zipcode2");
+        List<DepartmentDto> departmentDtoList = new ArrayList<>();
+        departmentDtoList.add(departmentDto);
+        departmentDtoList.add(departmentDto2);
+
+        List<DepartmentResponseModel> departmentList = utils.getDepartmentResponseModelList(departmentDtoList);
+
+        assertThat(departmentDtoList.get(0).getName()).isEqualTo(departmentList.get(0).getName());
+        assertThat(departmentDtoList.get(0).getCity()).isEqualTo(departmentList.get(0).getCity());
+        assertThat(departmentDtoList.get(0).getState()).isEqualTo(departmentList.get(0).getState());
+        assertThat(departmentDtoList.get(0).getCountry()).isEqualTo(departmentList.get(0).getCountry());
+        assertThat(departmentDtoList.get(0).getZipCode()).isEqualTo(departmentList.get(0).getZipCode());
+
+    }
+
+    @Test
+    public void getDepartmentResponseModelListNotFoundExceptionTest(){
+        List<DepartmentDto> list = null;
+
+        assertThatThrownBy(() -> utils.getDepartmentResponseModelList(list))
+                .isInstanceOf(NotFoundException.class)
+                .hasMessage("0 records found");
+    }
+
+    @Test
+    public void getDepartmentResponseModelListNotFoundExceptionTest2(){
+        List<DepartmentDto> list = new ArrayList<>();
+
+        assertThatThrownBy(() -> utils.getDepartmentResponseModelList(list))
+                .isInstanceOf(NotFoundException.class)
+                .hasMessage("0 records found");
+    }
+
+    @Test
+    public void getDepartmentResponseModelTest() throws NotFoundException {
+        DepartmentDto departmentDto = new DepartmentDto(1, "name","city","state","country","zip code");
+
+        DepartmentResponseModel departmentResponseModel = utils.getDepartmentResponseModel(departmentDto);
+
+        assertThat(departmentDto.getName()).isEqualTo(departmentResponseModel.getName());
+        assertThat(departmentDto.getCity()).isEqualTo(departmentResponseModel.getCity());
+        assertThat(departmentDto.getState()).isEqualTo(departmentResponseModel.getState());
+        assertThat(departmentDto.getCountry()).isEqualTo(departmentResponseModel.getCountry());
+        assertThat(departmentDto.getZipCode()).isEqualTo(departmentResponseModel.getZipCode());
+    }
+
+    @Test
+    public void getDepartmentResponseModelNotFoundExceptionTest(){
+        DepartmentDto departmentDto = null;
+        assertThatThrownBy(() -> utils.getDepartmentResponseModel(departmentDto))
+                .isInstanceOf(NotFoundException.class)
+                .hasMessage("Department not found");
+
+    }
+
+    @Test
+    public void getDepartmentDtoNotFoundExceptionTest(){
+        DepartmentRequestModel departmentDetails = null;
+
+        assertThatThrownBy(() -> utils.getDepartmentDto(departmentDetails))
+                .isInstanceOf(NotFoundException.class)
+                .hasMessage("Request object is null");
+    }
+
+    @Test
+    public void getDepartmentDtoListTest(){
+        DepartmentEntity departmentEntity = new DepartmentEntity(1, "name","city","state","country","zip code");
+
+        DepartmentEntity departmentEntity1 = new DepartmentEntity(1, "name","city","state","country","zip code");
+
+        Iterable<DepartmentEntity> departmentEntities = Arrays.asList(departmentEntity, departmentEntity1);
+
+        List<DepartmentDto> allDepartments = utils.getDepartmentDtoList(departmentEntities);
+
+        assertThat(allDepartments.get(0).getName()).isEqualTo(departmentEntity.getName());
+        assertThat(allDepartments.get(0).getCity()).isEqualTo(departmentEntity.getCity());
+        assertThat(allDepartments.get(0).getState()).isEqualTo(departmentEntity.getState());
+        assertThat(allDepartments.get(0).getCountry()).isEqualTo(departmentEntity.getCountry());
+        assertThat(allDepartments.get(0).getZipCode()).isEqualTo(departmentEntity.getZipCode());
+
+    }
+}
